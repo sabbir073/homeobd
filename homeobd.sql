faacaac39b4cca7b5529d5fa1be3ae37baf2bbda
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2019-08-14 16:19:16 +0600

    updated sql file

--- homeobd.sql
+++ homeobd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2019 at 04:05 PM
+-- Generation Time: Aug 14, 2019 at 11:16 AM
 -- Server version: 5.6.25
 -- PHP Version: 5.3.29
 
@@ -23,6 +23,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `medicines`
+--
+
+CREATE TABLE IF NOT EXISTS `medicines` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `shortform` varchar(255) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `subchapter` varchar(255) NOT NULL,
+  `source` varchar(255) NOT NULL,
+  `prover` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `medicines`
+--
+
+INSERT INTO `medicines` (`id`, `name`, `shortform`, `chapter`, `subchapter`, `source`, `prover`, `type`) VALUES
+(1, 'paracitamal', 'pcl', 'head', 'fever', 'Homeobd', 'Homeobd', 'Antibiotic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `weight` varchar(255) NOT NULL,
+  `height` varchar(255) NOT NULL,
+  `bp` varchar(255) NOT NULL,
+  `cell` varchar(255) NOT NULL,
+  `symptoms` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `name`, `age`, `gender`, `address`, `weight`, `height`, `bp`, `cell`, `symptoms`) VALUES
+(1, 'Motahar ali', '25', 'Male', 'Uttara, dhaka', '75', '5.6', '120/80', '01732036568', 'headche, flue, denger');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `symptoms`
+--
+
+CREATE TABLE IF NOT EXISTS `symptoms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `subchapter` varchar(255) NOT NULL,
+  `shortform` varchar(255) NOT NULL,
+  `relatedmedicine` varchar(10000) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `symptoms`
+--
+
+INSERT INTO `symptoms` (`id`, `name`, `chapter`, `subchapter`, `shortform`, `relatedmedicine`) VALUES
+(1, 'headache', 'head', 'normal', 'hd', 'paracitamol');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,18 +107,38 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` varchar(255) NOT NULL DEFAULT 'doctor',
   `credit` int(255) NOT NULL DEFAULT '20',
   `pending` varchar(255) NOT NULL DEFAULT 'false'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `token`, `role`, `credit`, `pending`) VALUES
-(1, 'Md Sabbir Ahmed', 'md.sabbir073@gmail.com', '67e4a0b2338a76fbb5bf7b85fced2e72', '', 'admin', 20, 'true');
+(1, 'Md Sabbir Ahmed', 'md.sabbir073@gmail.com', '67e4a0b2338a76fbb5bf7b85fced2e72', '', 'admin', 20, 'true'),
+(2, 'Raisa Islam Noushin', 'md.sabbir073@outlook.com', 'a1083f1528d8b91288ace29d4a250649', NULL, 'doctor', 20, 'false'),
+(3, 'Shabekunnaher Masuma', 'md.sabbir073@hotmail.com', '5114b8021ea68f2df4e057cf57c052ac', NULL, 'doctor', 20, 'false');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `medicines`
+--
+ALTER TABLE `medicines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`) COMMENT 'primary';
+
+--
+-- Indexes for table `symptoms`
+--
+ALTER TABLE `symptoms`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -59,10 +151,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `medicines`
+--
+ALTER TABLE `medicines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `symptoms`
+--
+ALTER TABLE `symptoms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
