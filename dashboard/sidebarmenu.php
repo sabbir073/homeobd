78aa71e5a590a293ec6a5f9d163a2a83e16ebd4a
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2019-08-16 02:32:00 +0600

    changing user avatar

--- dashboard/sidebarmenu.php
+++ dashboard/sidebarmenu.php
@@ -3,7 +3,7 @@
         <a href="index.php" class="az-logo">h<span>o</span>me<span>o</span>bd</a>
     </div><!-- az-sidebar-header -->
     <div class="az-sidebar-loggedin">
-        <div class="az-img-user online"><img src="./images/user.png" alt="User"></div>
+        <div class="az-img-user online"><img src="./images/user-white.png" alt="User"></div>
         <div class="media-body">
             <h6><?php echo $name;?></h6>
             <span><?php echo $role;?></span>
