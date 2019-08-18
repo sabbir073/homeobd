31a3383434495ee7f4e460b9f2a925b6b9b317a2
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2019-08-19 03:52:22 +0600

    added 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php include("header.php");?>
+
+<body class="az-body">
+    <div class="az-error-wrapper">
+      <h1>404</h1>
+      <h2>Oopps. The page you were looking for doesn't exist.</h2>
+      <h6>You may have mistyped the address or the page may have moved.</h6>
+      <a href="index.php" class="btn btn-outline-indigo">Back to Home</a>
+    </div>
+
+    <script src="./old/lib/jquery/jquery.min.js"></script>
+    <script src="./old/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./old/lib/ionicons/ionicons.js"></script>
+
+    <script src="./old/js/azia.js"></script>
+    <script>
+      $(function(){
+        'use strict'
+
+      });
+    </script>
+  </body>
+</html>
